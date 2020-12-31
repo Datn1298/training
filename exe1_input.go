a367628f729a239c7fdc320fecba98beeb51b1e7
DatNT <lfh275@gmail.com>
2020-12-31 09:19:44 -0800

    feature: hoan thien code

--- exe1_input.go
+++ exe1_input.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"fmt"
 	// "bufio"
-    // "fmt"
-    // "io"
-    "io/ioutil"
-    // "os"	
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	// "os"
+	// "strings"
 )
 
 const path = "./config.json"
+const path2 = "./config_2.json"
+const path3 = "./config_3.json"
 
 func check(e error) {
     if e != nil {
@@ -17,26 +19,136 @@ func check(e error) {
     }
 }
 
-// type Data struct {
-// 	type string
-// 	vCPU int
-// 	vRam int
-// 	counts int
-// }
+type Data struct {
+	Type   string `json:"type"`
+	VCPU   int    `json:"vCPU"`
+	VRam   int    `json:"vRAM"`
+	Counts int    `json:"counts"`
+	Action string
 
+}
 
-func main()  {
+type Output struct {
+	Type string
+	Action string
+	Counts int
+}
 
-	exe := path[len(path)-4: len(path)]
-
-	if exe == "json"{
-		data, err := ioutil.ReadFile(path)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Print(string(data))
-		
+func getProduct(path string) []Data {
+	
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 
-	
+	data2 := data[16: len(data)-1]
+
+	product := make([]Data, 3)
+	json.Unmarshal(data2, &product)
+	return product
 }
+ 
+var m = make(map[string]Data)
+
+func abc(path string, queue []Data) []Data {
+
+	products := getProduct(path)
+		
+	if len(queue) == 0 {
+		for i := range products {
+			queue = append(queue, products[i])
+			queue[i].Action = "a"
+		}	
+	} else {
+		for i := range products {
+			temp1 := products[i]
+
+			temp := queue[0]
+
+			queue = queue[1:]
+
+			if(temp.Counts < temp1.Counts){
+				temp = temp1
+				temp.Action = "provision"
+				queue = append(queue, temp)
+			} else {
+				temp = temp1
+				temp.Action = "delete"
+				queue = append(queue, temp)
+			}
+		}	
+	}
+	return queue
+}
+
+func main()  {
+	var queue []Data
+	var queue2 []Data
+
+	// reader := bufio.NewReader(os.Stdin)
+	// for ok := 1; ok != 2; {
+	// 	fmt.Print("Enter path: ")
+	// 	path, _ := reader.ReadString('\n')
+
+	// 	exe := path[len(path)-7: len(path)]
+	// 	exe = strings.TrimRight(exe, "\r\n")
+
+	// 	fmt.Println(exe)
+
+	// 	if exe == ".json" {
+	// 		queue = abc(path, queue)
+			
+	// 		for len(queue) > 0 {
+	// 			temp := queue[0]
+	// 			queue = queue[1:]   // Dequeue
+	// 			fmt.Println(temp.Type , " " , temp.Action , " " , temp.Counts)
+	// 			fmt.Println("===============")
+	// 			queue2 = append(queue2, temp)
+	// 		}
+
+	// 		queue = queue2
+	// 	} else {
+	// 		fmt.Println("Invalid file! Enter path again: ")
+	// 	}
+	// }
+
+
+	queue = abc(path, queue)
+
+	for len(queue) > 0 {
+		temp := queue[0]
+		queue = queue[1:]   // Dequeue
+		fmt.Println(temp.Type , " " , temp.Action , " " , temp.Counts)
+		fmt.Println("===============")
+		queue2 = append(queue2, temp)
+	}
+
+	queue = queue2
+
+	queue = abc(path2, queue)
+
+	for len(queue) > 0 {
+		temp := queue[0]
+		queue = queue[1:]   // Dequeue
+		fmt.Println(temp.Type , " " , temp.Action , " " , temp.Counts)
+		fmt.Println("===============")
+		queue2 = append(queue2, temp)
+	}
+
+	queue = queue2
+
+	queue = abc(path3, queue)
+
+	for len(queue) > 0 {
+		temp := queue[0]
+		queue = queue[1:]   // Dequeue
+		fmt.Println(temp.Type , " " , temp.Action , " " , temp.Counts)
+		fmt.Println("===============")
+		queue2 = append(queue2, temp)
+	}
+
+
+
+}
+
+
